22962f06246d3456495713c5b17878acd33bfc06
Mira Kaczmarek <m.kaczmarek@npc.pl>
2021-12-20 15:00:04 +0100

    Added module 9

---
+++ src/Module9_Interface/Music.java
@@ -0,0 +1,19 @@
+package Module9_Interface;
+
+public interface Music {
+    //1. nie można tworzyć pól obiektu
+    //2. można tworzyć stałe - statyczne,finalne
+
+     String NAME = "MUSIC"; // stała public static final ->zawsze!
+
+    //3. tylko metody abstrakcyjne
+
+     void playMusic(); //zawsze będą = public abstract
+    void pauseMusic();
+    void stopMusic();
+
+    default void sayHelloFromMusic(){
+        System.out.println("Hello from default");
+    }
+
+}
